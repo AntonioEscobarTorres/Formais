82747e35ed805426a7fab1143ef49953ae7b0027
Antônio Torres <antonio.escobar.torres11@gmail.com>
2025-05-29 15:44:22 -0300

    mais um teste

--- teste_analisador.py
+++ teste_analisador.py
@@ -2,7 +2,8 @@ from AnalisadorLexico import AnalisadorLexico
 
 expressoes = [
     ("NUM", "a*"),
-    ("AB_TOKEN", "ab")
+    ("AB_TOKEN", "ab"),
+    ("BnoINICIO", "b(a|b)*")
 ]
 
 
@@ -12,3 +13,4 @@ analisador = AnalisadorLexico(expressoes)
 print(analisador.reconhecer_token("aaaaaa"))  # Deve retornar NUM
 print(analisador.reconhecer_token("ab"))   # Deve retornar AB_TOKEN
 print(analisador.reconhecer_token("b"))    # Deve retornar "Token não reconhecido"
+print(analisador.reconhecer_token("baaaab"))    # Deve retornar BnoINICIO"
